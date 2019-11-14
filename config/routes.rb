474c090c5d10eb20b08977aff4d13363e7736004
Sang Luong <tansang020798@gmail.com>
2019-11-14 22:02:22 +0700

    push source login_app to github

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "login" => "session#new"
+  post    "login"    => "session#create"
+  delete  "logout"   => "session#destroy"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+end
